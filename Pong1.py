b956328c3085ea500dc8c301b743ef1db8a7948f
Oleksii Fedorenko <alex4rest1@gmail.com>
2022-08-03 19:32:39 +0300

    created window for python-game

---
+++ Pong1.py
@@ -0,0 +1,17 @@
+# Simple Pong in Python 3 for Beinners
+# By @TokyoEdTech
+# Part 1: Getting Started
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("Ping by @TokyoEdTech")
+wn.bgcolor("green")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+
+
+# Main game loop
+while True:
+    wn.update()
