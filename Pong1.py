8327f91d3c54604d2152565ee2ff5c4e56ceda4e
Oleksii Fedorenko <alex4rest1@gmail.com>
2022-08-04 21:19:57 +0300

    added sounds and score

--- Pong1.py
+++ Pong1.py
@@ -2,13 +2,19 @@
 # By @TokyoEdTech
 # Part 1: Getting Started
 
+from asyncore import write
 import turtle
+import winsound
 
 wn = turtle.Screen()
 wn.title("Ping by @TokyoEdTech")
 wn.bgcolor("grey")
 wn.setup(width=800, height=600)
 wn.tracer(0)
+
+# Score
+score_a = 0
+score_b = 0
 
 # Paddle A
 paddle_a = turtle.Turtle()
@@ -40,27 +46,39 @@ ball.dx = 0.1
 ball.dy = -0.1
 
 
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0 Player B: 0", align="center",
+          font=("Courier", 24, "normal"))
+
 # Function
 def paddle_a_up():
     y = paddle_a.ycor()
     y += 20
     paddle_a.sety(y)
 
+
 def paddle_a_down():
     y = paddle_a.ycor()
     y += -20
     paddle_a.sety(y)
+
 
 def paddle_b_up():
     y = paddle_b.ycor()
     y += 20
     paddle_b.sety(y)
 
+
 def paddle_b_down():
     y = paddle_b.ycor()
     y += -20
     paddle_b.sety(y)
-
 
 
 # Keyboard binding
@@ -82,10 +100,12 @@ while True:
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
+        winsound.PlaySound("bounce_wall.wav", winsound.SND_ASYNC)
 
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
+        winsound.PlaySound("bounce_wall.wav", winsound.SND_ASYNC)
 
     if ball.ycor() < -290:
         ball.sety(-290)
@@ -94,16 +114,29 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        winsound.PlaySound("energy-bounce.wav", winsound.SND_ASYNC)
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b),
+                  align="center", font=("Courier", 24, "normal"))
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+        winsound.PlaySound("energy-bounce.wav", winsound.SND_ASYNC)
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b),
+                  align="center", font=("Courier", 24, "normal"))
 
-    # Paddle adn balls collisions
+    # Paddle and balls collisions
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
         ball.setx(340)
         ball.dx *= -1
+        winsound.PlaySound("bounce_1.wav", winsound.SND_ASYNC)
 
     if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
         ball.setx(-340)
         ball.dx *= -1
+        winsound.PlaySound("bounce_1.wav", winsound.SND_ASYNC)
+ц
